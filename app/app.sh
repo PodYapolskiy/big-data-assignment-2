8bf0f2effe4875b35099e954acc389c320640a73
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-04-13 13:53:31 +0300

    chores: rewrite env usage using uv and update requitements with versions

--- app/app.sh
+++ app/app.sh
@@ -5,27 +5,26 @@ service ssh restart
 # Starting the services
 bash start-services.sh
 
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-# source $HOME/.local/bin/env
-# uv venv --python 3.12
+# using uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 
 # Creating a virtual environment
-python3 -m venv .venv
+uv venv --python 3.12
+# python3 -m venv .venv
 source .venv/bin/activate
 
 # Install any packages
-# uv ...
-pip install -r requirements.txt  
+uv pip install -r requirements.txt
 
 # Package the virtual env.
-venv-pack -o .venv.tar.gz
+uvx venv-pack -o .venv.tar.gz
 
 # Collect data
-bash prepare_data.sh
-
+# bash prepare_data.sh
 
 # Run the indexer
-bash index.sh data/sample.txt
+# bash index.sh data/sample.txt
 
 # Run the ranker
-bash search.sh "this is a query!"
+# bash search.sh "this is a query!"
